d1fc4d385b07246e6b745eb6be7a3eef001d9c91
Persie <hashcod3@protonmail.com>
2022-02-14 18:52:42 +0100

    fixed wrong cred login

--- login.php
+++ login.php
@@ -1,98 +1,71 @@
-                            <?php
-                            session_start();
+<?php
+session_start();
 
-                            include "connect.php";
+include "connect.php";
 
-                            if (
-                                isset($_POST["txt_email"]) &&
-                                isset($_POST["txt_pwd"])) 
-                            {
-                                $email = $_POST["txt_email"];
+if (isset($_POST["txt_email"]) && isset($_POST["txt_pwd"])) {
+    $email = $_POST["txt_email"];
+    
+    $pass = $_POST["txt_pwd"];
+    
+    if (empty($email)) {
+        $_SESSION["error"] = 'Email is required';
+        header("Location: index.php");
+        exit();
+    } elseif (empty($pass)) {
+        $_SESSION["error"] = 'Password is required';
+        header("Location: index.php");
+        exit();
+    } else {
+        if ($email === "admin" && $pass === "1234") {
+            $_SESSION["Email"]    = "admin";
+            $_SESSION["Passwort"] = "1234";
+            $_SESSION["Mode"]     = "Admin";
+            header("Location: admin.php");
+        } else {
+            $sql = "SELECT * FROM arbeiter WHERE Email='$email' AND Passwort='$pass'";
+            
+            $result = mysqli_query($conn, $sql);
+            
+            if (mysqli_num_rows($result) === 1) {
+                $row = mysqli_fetch_assoc($result);
+                    
+                    $_SESSION["Email"] = $row["Email"];
+                    
+                    $_SESSION["Passwort"] = $row["Passwort"];
+                    
+                    $_SESSION["Mode"] = "Arbeiter";
+                    header("Location: arbeiter.php");
+                    exit();
+            } else {
+                $sql = "SELECT * FROM kunde WHERE Email='$email' AND Passwort='$pass'";
+                
+                $result = mysqli_query($conn, $sql);
+                
+                if (mysqli_num_rows($result) === 1) {
+                    $row = mysqli_fetch_assoc($result);
+                        
+                        $_SESSION["Email"] = $row["Email"];
+                        
+                        $_SESSION["Passwort"] = $row["Passwort"];
+                        
+                        $_SESSION["Mode"] = "Kunde";
+                        header("Location: kunde.php");
+                        exit();
 
-                                $pass = $_POST["txt_pwd"];
-
-                                if (empty($email)) {
-                                    header("Email is required");
-
-                                    exit();
-                                } elseif (empty($pass)) {
-                                    header("Password is required");
-
-                                    exit();
-                                } else {
-                                    if(($email==='admin') && ($pass==='1234'))
-                                    {
-                                        $_SESSION["Email"] = 'admin';
-                                         $_SESSION["Passwort"] = '1234';
-                                        $_SESSION["Mode"] = "Admin";
-                                        header("Location: admin.php");
-                                    }
-                                    else{
-                                    $sql = "SELECT * FROM arbeiter WHERE Email='$email' AND Passwort='$pass'";
-
-                                    $result = mysqli_query($conn, $sql);
-
-                                    if (mysqli_num_rows($result) === 1) {
-                                        $row = mysqli_fetch_assoc($result);
-
-                                        if (
-                                            $row["Email"] === $email &&
-                                            $row["Passwort"] === $pass
-                                        ) {
-                                            echo "Logged in!";
-
-                                            $_SESSION["Email"] = $row["Email"];
-
-                                            $_SESSION["Passwort"] =
-                                                $row["Passwort"];
-                                            
-                                            $_SESSION["Mode"] = "Arbeiter";
-                                            header("Location: arbeiter.php");
-                                            exit();
-                                        } else {
-                                            $_SESSION["error"] =
-                                                "username/password incorrect";
-                                            header("Location: index.php");
-                                            exit();
-                                        }
-                                    } else {
-                                        $sql = "SELECT * FROM kunde WHERE Email='$email' AND Passwort='$pass'";
-
-                                        $result = mysqli_query($conn, $sql);
-
-                                        if (mysqli_num_rows($result) === 1) {
-                                            $row = mysqli_fetch_assoc($result);
-
-                                            if (
-                                                $row["Email"] === $email &&
-                                                $row["Passwort"] === $pass
-                                            ) {
-                                                echo "Logged in!";
-
-                                                $_SESSION["Email"] =
-                                                    $row["Email"];
-
-                                                $_SESSION["Passwort"] =
-                                                    $row["Passwort"];
-                                                
-                                            $_SESSION["Mode"] = "Kunde";
-                                                header("Location: kunde.php");
-                                                exit();
-                                            } else {
-                                                $_SESSION["error"] =
-                                                    "username/password incorrect";
-                                                header("Location: index.php");
-                                                exit();
-                                            }
-                                        }
-                                    }
-                                }
-                                }
-                            } else {
-                                $_SESSION["error"] =
-                                    "username/password incorrect";
-                                header("Location: index.php");
-                            }
+                }
+                else {
+                        $_SESSION["error"] = "username/password incorrect";
+                        header("Location: index.php");
+                        exit();
+                    }
+            }
+        }
+    }
+} else {
+    $_SESSION["error"] = "username/password incorrect";
+    header("Location: index.php");
+}
 
 
 ?>
