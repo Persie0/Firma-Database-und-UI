07a02b4ae350a408af5a2ed4041c7227519ae1bd
Persie <hashcod3@protonmail.com>
2022-03-30 16:28:00 +0200

    added phone

--- pages/index.php
+++ pages/index.php
@@ -35,7 +35,7 @@ session_start();
                             ?>
                         </div>
                         <div class="forget-pass">
-                            <a href="mailto:marek@mail.com">Forgot password?</a>
+                            <a href="tel:555-666-7777">Forgot password?</a>
                         </div>
                         <button type="submit">Login</button>
 
